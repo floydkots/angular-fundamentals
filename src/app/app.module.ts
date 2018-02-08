ea21d368fdbf32a4a75a3ba2f95461ecd13ba8c8
Floyd Kots <floydkotohoyoh@gmail.com>
2018-02-08 14:52:25 +0300

    Register the server module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
